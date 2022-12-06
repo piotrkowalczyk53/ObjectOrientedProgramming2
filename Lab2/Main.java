53f3bbda81e7dcd28b7fb1aa971b3aa3ad8afe5f
Piotr Kowalczyk <92885222+piotrkowalczyk53@users.noreply.github.com>
2022-12-06 22:16:52 +0100

    Finish task
    Implemented points 2 & 3

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -1,42 +1,44 @@
+package Lab2;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
-class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.print("  " + LocalDate.now().getMonth().getDisplayName(TextStyle.FULL, new Locale("pl")) + " ");
-        System.out.println(LocalDate.now().getYear());
-        for (int i = 1; i <= 7; i++)
-        {
-            System.out.print("  " + DayOfWeek.of(i).getDisplayName(TextStyle.NARROW_STANDALONE, new Locale("pl")));
+class Main {
+    public static void main(String[] args) {
+
+        //set date to that requested by the user
+        LocalDate date = LocalDate.now();
+        if (args.length == 2) {
+            date = LocalDate.now().withMonth(Integer.parseInt(args[1]));
+        }
+        if (args.length == 4) {
+            date = LocalDate.of(Integer.parseInt(args[3]), Integer.parseInt(args[1]), 1);
+        }
+
+        System.out.printf("%13s", date.getMonth().getDisplayName(TextStyle.FULL_STANDALONE, Locale.forLanguageTag("pl")));
+        System.out.println(" " + date.getYear());
+        for (int i = 1; i <= 7; i++) {
+            System.out.print("  " + DayOfWeek.of(i).getDisplayName(TextStyle.NARROW_STANDALONE, Locale.forLanguageTag("pl")));
         }
         System.out.println();
-        int length = LocalDate.now().getMonth().length(false);
-        int today = LocalDate.now().getDayOfMonth();
-        int start = LocalDate.now().minusDays(today).getDayOfWeek().getValue() + 1;
-        int temp = start;
-        for (int i = 1; i < start; i++)
-        {
+        int length = date.lengthOfMonth();
+        int today = date.getDayOfMonth();
+        int position = date.withDayOfMonth(1).getDayOfWeek().getValue(); //gets numeric value of day of the week current moth started on
+        for (int i = 1; i < position; i++) {
             System.out.print("   ");
         }
-        for (int i = 1; i < length; i++)
-        {
-            if (i == today)
-            {
+        for (int i = 1; i <= length; i++) {
+            if (i == today && args.length == 0) {
                 System.out.printf(" %d*", i);
-            }
-            else
-            {
+            } else {
                 System.out.printf("%3d", i);
             }
-            if (temp % 7 == 0)
-            {
+            if (position % 7 == 0) {
                 System.out.println();
             }
-            temp++;
+            position++;
         }
     }
 }
